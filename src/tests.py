f365785fffa48ef12d7f88b0ad8be24c55adfeea
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-07 21:37:30 -0500

    Add test for reset_cells_visited method of class maze

--- src/tests.py
+++ src/tests.py
@@ -21,3 +21,14 @@ class Tests(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(len(m1._cells), num_cols)
         self.assertEqual(len(m1._cells[0]), num_rows)
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
